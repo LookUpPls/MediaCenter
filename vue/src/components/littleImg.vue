8a297e26ce58e5654d6531b6a7374b540090039f
crimson <1357885013@qq.com>
2021-09-25 19:11:59 +0800

    相册 右边右空白

--- vue/src/components/littleImg.vue
+++ vue/src/components/littleImg.vue
@@ -1,78 +1,79 @@
 <template>
-    <div :style="style" >
-        <img :src="src" alt="" class="img" ref="image">
-    </div>
+  <div :style="style">
+    <img :src="src" alt="" class="img" ref="image">
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "littleImg",
-        props: {
-            name: String,
-            src: String,
-            gridSize: [Number, String],
-            blockSize: [Number, String]
-        },
-        data: function () {
-            return {
-                style: {
-                    gridRow: "span 1",
-                    gridColumn: "span 1",
-                }
-            }
-        },
-        mounted() {
-            this.getImgNaturalDimensions(this.$refs.image, this, function (othis, dimension) {
-                // console.log(othis);
-                // console.log(othis.src);
-                // console.log(dimension);
-                if (dimension.h >= dimension.w) {
-                    othis.style.gridColumn = "span " + parseInt(othis.blockSize / othis.gridSize);
-                    othis.style.gridRow = "span " + parseInt(othis.blockSize * dimension.h / dimension.w / othis.gridSize);
-                } else {
-                    othis.style.gridRow = "span " + parseInt(othis.blockSize / othis.gridSize);
-                    othis.style.gridColumn = "span " + parseInt(othis.blockSize * dimension.w / dimension.h / othis.gridSize);
-                }
-                var scale = dimension.w / dimension.h;
-                console.log(scale);
-                //
-                // if (scale > 1.5)
-                //     othis.height = othis.gridSize * 2 + "px";
-                // else
-                //     othis.height = othis.gridSize + "px";
-            })
-        },
-        methods: {
-            getImgNaturalDimensions: function (oImg, othis, callback) {
-                // console.log(oImg)
-                var nWidth, nHeight;
-                if (oImg.naturalWidth) { // 现代浏览器
-
-                    nWidth = oImg.naturalWidth;
-                    nHeight = oImg.naturalHeight;
-                    callback(othis, {w: nWidth, h: nHeight});
-
-                } else { // IE6/7/8
-                    var nImg = new Image();
-
-                    nImg.onload = function () {
-                        var nWidth = nImg.width,
-                            nHeight = nImg.height;
-                        callback(othis, {w: nWidth, h: nHeight});
-                    };
-                    nImg.src = oImg.src;
-                }
-            }
-        }
+export default {
+  name: "littleImg",
+  props: {
+    name: String,
+    src: String,
+    gridSize: [Number, String],
+    blockSize: [Number, String]
+  },
+  data: function () {
+    return {
+      style: {
+        gridRow: "span 1",
+        gridColumn: "span 1",
+      }
     }
+  },
+  mounted() {
+    this.getImgNaturalDimensions(this.$refs.image, this,
+        function (othis, dimension) {
+          // console.log(othis);
+          // console.log(othis.src);
+          // console.log(dimension);
+          if (dimension.h >= dimension.w) {
+            othis.style.gridColumn = "span " + parseInt(othis.blockSize / othis.gridSize);
+            othis.style.gridRow = "span " + parseInt(othis.blockSize * dimension.h / dimension.w / othis.gridSize);
+          } else {
+            othis.style.gridRow = "span " + parseInt(othis.blockSize / othis.gridSize);
+            othis.style.gridColumn = "span " + parseInt(othis.blockSize * dimension.w / dimension.h / othis.gridSize);
+          }
+          var scale = dimension.w / dimension.h;
+          console.log(scale);
+          //
+          // if (scale > 1.5)
+          //     othis.height = othis.gridSize * 2 + "px";
+          // else
+          //     othis.height = othis.gridSize + "px";
+        })
+  },
+  methods: {
+    getImgNaturalDimensions: function (oImg, othis, callback) {
+      // console.log(oImg)
+      var nWidth, nHeight;
+      if (oImg.naturalWidth) { // 现代浏览器
+
+        nWidth = oImg.naturalWidth;
+        nHeight = oImg.naturalHeight;
+        callback(othis, {w: nWidth, h: nHeight});
+
+      } else { // IE6/7/8
+        var nImg = new Image();
+
+        nImg.onload = function () {
+          var nWidth = nImg.width,
+              nHeight = nImg.height;
+          callback(othis, {w: nWidth, h: nHeight});
+        };
+        nImg.src = oImg.src;
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
-    .img {
-        width: 100%;
-        height: 100%;
-        /*object-fit: cover;*/
-        /*border: 1px solid #666666;*/
-    }
+.img {
+  width: 100%;
+  height: 100%;
+  /*object-fit: cover;*/
+  /*border: 1px solid #666666;*/
+}
 
 </style>
--- vue/src/views/Album.vue
+++ vue/src/views/Album.vue
@@ -1,132 +1,134 @@
 <template>
-    <div class="home">
-        <div>
-            <p class="title">文件夹<span>共4个</span></p>
-            <div class="folders">
-                <folder1 :name="folder.name" :src="folder.src" v-for="folder in folderData"></folder1>
-            </div>
-        </div>
-        <div>
-            <p class="title">图片<span>共21个</span></p>
-            <div class="imgs" v-bind:style="{ display: 'grid', 'grid-template-rows': 'repeat(auto-fill,16px)', 'grid-template-columns': 'repeat(auto-fill,16px)',}">
-                <littleImg @command="onCommand" :blockSize="blockSize"  :gridSize="gridSize" :src="folder.src" name="folder.name" v-for="folder in folderData"></littleImg>
-            </div>
-        </div>
-        <a-button @click="testMock1">{{testMockVal}}</a-button>
+  <div class="home">
+    <div>
+      <p class="title">文件夹<span>共4个</span></p>
+      <div class="folders">
+        <folder1 :name="folder.name" :src="folder.src" v-for="folder in folderData"></folder1>
+      </div>
     </div>
+    <div>
+      <p class="title">图片<span>共21个</span></p>
+      <div class="imgs"
+           v-bind:style="{ display: 'grid', 'grid-template-rows': 'repeat(auto-fill,16px)', 'grid-template-columns': 'repeat(auto-fill,16px)',}">
+        <littleImg @command="onCommand" :blockSize="blockSize" :gridSize="gridSize" :src="folder.src" name="folder.name"
+                   v-for="folder in folderData"></littleImg>
+      </div>
+    </div>
+    <a-button @click="testMock1">{{ testMockVal }}</a-button>
+  </div>
 </template>
 
 <script>
-    var axios = require('axios');
-    import {testMock} from '../api/testMock'
-    import {getDir} from '../api/album'
+var axios = require('axios');
+import {testMock} from '../api/testMock'
+import {getDir} from '../api/album'
 
-    import folder1 from '@/components/folder1.vue'
-    import littleImg from '@/components/littleImg.vue'
+import folder1 from '@/components/folder1.vue'
+import littleImg from '@/components/littleImg.vue'
 
-    export default {
-        name: 'home',
-        data: function () {
-            return {
-                gridSize: 28,
-                testMockVal: "test",
-                folderData: {},
-                picData: {},
-                gridStyle: {}
-            }
-        },
-        components: {
-            folder1,
-            littleImg
-        },
-        computed: {
-            blockSize: function () {
-                let size = 312;
-                let yv=size%this.gridSize;
-                if(yv>this.gridSize/2)
-                    size+=yv;
-                else
-                    size-=yv;
-                console.log(size)
-                return size
-            }
-        },
-        created() {
-            getDir().then(response => {
-                this.$data.folderData = response.data.folderData;
-                this.$data.picData = response.data.picData;
-            }).catch(err => {
-                console.log(err);
-                // reject(false);
-            })
-        },
-        methods: {
-            onCommand:function(info){
-                switch (info.order) {
-                    case "closeVideo":
-                        break;
-                }
-            },
-            testMock1: function () {
-                this.testMockVal = "clicked";
-                console.log("clicked");
-                testMock().then(response => {
-                    console.log(JSON.stringify(response.data));
-
-                }).catch(err => {
-                    console.log(err)
-                    reject(false)
-                })
-            }
-        }
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      gridSize: 28,
+      testMockVal: "test",
+      folderData: {},
+      picData: {},
+      gridStyle: {}
     }
+  },
+  components: {
+    folder1,
+    littleImg
+  },
+  computed: {
+    blockSize: function () {
+      let size = 312;
+      let yv = size % this.gridSize;
+      if (yv > this.gridSize / 2)
+        size = size + this.gridSize - yv;
+      else
+        size -= yv;
+      console.log(size)
+      return size
+    }
+  },
+  created() {
+    getDir().then(response => {
+      this.$data.folderData = response.data.folderData;
+      this.$data.picData = response.data.picData;
+    }).catch(err => {
+      console.log(err);
+      // reject(false);
+    })
+  },
+  methods: {
+    onCommand: function (info) {
+      switch (info.order) {
+        case "closeVideo":
+          break;
+      }
+    },
+    testMock1: function () {
+      this.testMockVal = "clicked";
+      console.log("clicked");
+      testMock().then(response => {
+        console.log(JSON.stringify(response.data));
+
+      }).catch(err => {
+        console.log(err)
+        reject(false)
+      })
+    }
+  }
+}
 </script>
 
 <style>
-    .folders {
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: flex-start;
+.folders {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
 
-        margin-bottom: 30px;
+  margin-bottom: 30px;
 
-    }
+}
 
-    .imgs {
-        margin: 10px;
-        grid-auto-flow: row dense;
-        background-color: aliceblue;
-    }
+.imgs {
+  margin: 10px;
+  grid-auto-flow: row dense;
+  background-color: aliceblue;
+}
 
-    .imgs > img {
-        /*float: left;*/
-    }
+.imgs > img {
+  /*float: left;*/
+}
 
-    .folders > div {
-        float: left;
-        margin: 10px;
-    }
+.folders > div {
+  float: left;
+  margin: 10px;
+}
 
-    .title {
-        font-family: 'Microsoft Tai Le Bold', 'Microsoft Tai Le Regular', 'Microsoft Tai Le';
-        font-weight: 700;
-        font-style: normal;
-        font-size: 20px;
-        color: rgba(0, 0, 0, 0.847058823529412);
-        line-height: 28px;
-        letter-spacing: 2px;
+.title {
+  font-family: 'Microsoft Tai Le Bold', 'Microsoft Tai Le Regular', 'Microsoft Tai Le';
+  font-weight: 700;
+  font-style: normal;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.847058823529412);
+  line-height: 28px;
+  letter-spacing: 2px;
 
-        margin-bottom: 5px;
-    }
+  margin-bottom: 5px;
+}
 
-    .title > span {
-        margin-left: 8px;
-        font-family: 'Microsoft Tai Le';
-        font-weight: 400;
-        font-style: normal;
-        font-size: 14px;
-        color: rgba(0, 0, 0, 0.447058823529412);
-        line-height: 22px;
-    }
+.title > span {
+  margin-left: 8px;
+  font-family: 'Microsoft Tai Le';
+  font-weight: 400;
+  font-style: normal;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.447058823529412);
+  line-height: 22px;
+}
 </style>
